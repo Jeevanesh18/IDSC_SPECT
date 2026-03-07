57b2ec6fef473a252437d9bb7b0179261d1dd4a4
Jeevanesh18 <jeeva7396@gmail.com>
2026-03-07 23:13:04 +0800

    Update prediction script with new parameters

--- predict.sh
+++ predict.sh
@@ -2,9 +2,10 @@
 
 # Predict using trained weights and save probabilities
 nnUNetv2_predict \
-    -i /app/nnUNet_raw/Dataset999_SPECT/imagesTs \   # input images
-    -o /app/nnUNet_results/Dataset999_SPECT/predictions \   # output folder
-    -tr nnUNetTrainerV2 \   # trainer class (uses the trained weights in nnUNet_results)
-    -m 2d \   # 2D nnU-Net model
-    -f 0 \    # fold 0
-    --save_probabilities   # save probability maps in addition to masks
+    -i /app/nnUNet_raw/Dataset999_SPECT/imagesTs \
+    -o /app/nnUNet_results/Dataset999_SPECT/predictions \
+    -d 999 \
+    -c 2d \
+    -tr nnUNetTrainerV2 \
+    -f 0 \
+    --save_probabilities \
