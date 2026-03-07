c0512775e6680758b68386705ac6f02df978ad6e
Jeevanesh18 <jeeva7396@gmail.com>
2026-03-07 23:38:57 +0800

    Update prediction script to include checkpoint and device

--- predict.sh
+++ predict.sh
@@ -1,11 +1,9 @@
-#!/bin/bash
-
-# Predict using trained weights and save probabilities
 nnUNetv2_predict \
     -i /app/nnUNet_raw/Dataset999_SPECT/imagesTs \
     -o /app/nnUNet_results/Dataset999_SPECT/predictions \
     -d 999 \
     -c 2d \
-    -tr nnUNetTrainerV2 \
     -f 0 \
-    --save_probabilities \
+    -chk checkpoint_best.pth \
+    --save_probabilities -device cpu
+    
