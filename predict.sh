e9e8b3bf8edd987f623f387d59c62667e02db9fa
Jeevanesh18 <jeeva7396@gmail.com>
2026-03-09 23:31:58 +0800

    Update predict.sh
    Preventing separate line errors

--- predict.sh
+++ predict.sh
@@ -1,9 +1,1 @@
-nnUNetv2_predict \
-    -i /app/nnUNet_raw/Dataset999_SPECT/imagesTs \
-    -o /app/nnUNet_results/Dataset999_SPECT/predictions \
-    -d 999 \
-    -c 2d \
-    -f 0 \
-    -chk checkpoint_best.pth \
-    --save_probabilities -device cpu
-    
+nnUNetv2_predict \ -i /app/nnUNet_raw/Dataset999_SPECT/imagesTs \ -o /app/nnUNet_results/Dataset999_SPECT/predictions \ -d 999 \ -c 2d \ -f 0 \ -chk checkpoint_best.pth \ --save_probabilities -device cpu
